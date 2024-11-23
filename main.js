9caca648792149e582b3b0deb841f3681cc2c01b
aniosu <ameniosu@au.com>
2024-11-23 19:44:00 +0900

    ほぼ完成

---
+++ main.js
@@ -0,0 +1,51 @@
+// 各要素を取得
+const item1 = document.getElementById("item1");
+const item2 = document.getElementById("item2");
+const item3 = document.getElementById("item3");
+const item4 = document.getElementById("item4");
+const item5 = document.getElementById("item5");
+const item0 = document.getElementById("item0");
+const content0 = document.getElementById("content0");
+const content1 = document.getElementById("content1");
+const content2 = document.getElementById("content2");
+const content3 = document.getElementById("content3");
+const content4 = document.getElementById("content4");
+const content5 = document.getElementById("content5");
+
+// 全てのコンテンツを非表示にする関数
+function hideAllContents() {
+  content1.classList.remove("active");
+  content2.classList.remove("active");
+  content3.classList.remove("active");
+  content4.classList.remove("active");
+  content5.classList.remove("active");
+  content0.classList.remove("active");
+}
+
+// 各要素にクリックイベントを設定
+item1.onclick = function () {
+  hideAllContents();
+  content1.classList.add("active");
+};
+
+item2.onclick = function () {
+  hideAllContents();
+  content2.classList.add("active");
+};
+
+item3.onclick = function () {
+  hideAllContents();
+  content3.classList.add("active");
+};
+item4.onclick = function () {
+  hideAllContents();
+  content4.classList.add("active");
+};
+item5.onclick = function () {
+  hideAllContents();
+  content5.classList.add("active");
+};
+item0.onclick = function () {
+  hideAllContents();
+  content0.classList.add("active");
+};
